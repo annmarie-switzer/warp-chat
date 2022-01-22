09404363dc4180a738f74027643db7c2288093ed
Annmarie Switzer <aargiros@gmail.com>
2022-01-21 23:05:56 -0500

    chat rooms work[C

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use futures_util::{SinkExt, StreamExt, TryFutureExt};
-use std::collections::HashMap;
 use std::env;
 use std::sync::{
     atomic::{AtomicUsize, Ordering},
@@ -10,14 +9,15 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 use warp::ws::{Message, WebSocket};
 use warp::Filter;
 
-/// Our global unique user id counter.
+struct User {
+    id: usize,
+    room: usize,
+    tx: mpsc::UnboundedSender<Message>,
+}
+
 static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
-/// Our state of currently connected users.
-///
-/// - Key is their id
-/// - Value is a sender of `warp::ws::Message`
-type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+type Users = Arc<RwLock<Vec<User>>>;
 
 #[tokio::main]
 async fn main() {
@@ -28,35 +28,35 @@ async fn main() {
     pretty_env_logger::init();
 
     let users = Users::default();
+
+    // custom filter
     let users = warp::any().map(move || users.clone());
 
+    // GET `/chat/:room_id`
+    // Called from the browser when the page loads
     let chat = warp::path("chat")
         .and(warp::ws())
-        .and(warp::path::param::<u32>())
+        .and(warp::path::param::<usize>())
         .and(users)
-        .map(|ws: warp::ws::Ws, room_id, users| 
+        .map(|ws: warp::ws::Ws, room_id, users| {
             ws.on_upgrade(move |socket| user_connected(socket, room_id, users))
-        );
+        });
 
-    let index = warp::path::end().map(|| warp::reply::html(INDEX_HTML));
+    // GET `/:room_id`
+    let index = warp::path!(usize).map(|_| warp::reply::html(INDEX_HTML));
 
     let routes = index.or(chat);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-async fn user_connected(ws: WebSocket, room_id: u32, users: Users) {
-    // Use a counter to assign a new unique ID for this user.
+async fn user_connected(ws: WebSocket, room_id: usize, users: Users) {
     let user_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-    eprintln!("Room number: {room_id}");
-    eprintln!("New chat user: {user_id}");
+    eprintln!("New chat user: {user_id} added to room: {room_id}");
 
-    // Split the socket into a sender and receive of messages.
     let (mut user_ws_tx, mut user_ws_rx) = ws.split();
 
-    // Use an unbounded channel to handle buffering and flushing of messages
-    // to the websocket...
     let (tx, rx) = mpsc::unbounded_channel();
     let mut rx = UnboundedReceiverStream::new(rx);
 
@@ -71,14 +71,16 @@ async fn user_connected(ws: WebSocket, room_id: u32, users: Users) {
         }
     });
 
-    // Save the sender in our list of connected users.
-    users.write().await.insert(user_id, tx);
+    let new_user = User {
+        id: user_id,
+        room: room_id,
+        tx,
+    };
 
-    // Return a `Future` that is basically a state machine managing
-    // this specific user's connection.
+    users.write().await.push(new_user);
 
-    // Every time the user sends a message, broadcast it to
-    // all other users...
+    // Every time the user sends a message,
+    // broadcast it to all other users in the same room.
     while let Some(result) = user_ws_rx.next().await {
         let msg = match result {
             Ok(msg) => msg,
@@ -87,23 +89,19 @@ async fn user_connected(ws: WebSocket, room_id: u32, users: Users) {
                 break;
             }
         };
-        user_message(user_id, msg, &users).await;
+
+        user_message(user_id, msg, &users, room_id).await;
     }
 
-    // user_ws_rx stream will keep processing as long as the user stays
-    // connected. Once they disconnect, then...
     user_disconnected(user_id, &users).await;
 }
 
 async fn user_disconnected(user_id: usize, users: &Users) {
     eprintln!("good bye user: {user_id}");
-
-    // Stream closed up, so remove from the user list
-    users.write().await.remove(&user_id);
+    users.write().await.retain(|u| u.id != user_id);
 }
 
-async fn user_message(user_id: usize, msg: Message, users: &Users) {
-    // Skip any non-Text messages...
+async fn user_message(user_id: usize, msg: Message, users: &Users, room_id: usize) {
     let msg = if let Ok(s) = msg.to_str() {
         s
     } else {
@@ -113,9 +111,9 @@ async fn user_message(user_id: usize, msg: Message, users: &Users) {
     let new_msg = format!("<User#{user_id}>: {msg}");
 
     // New message from this user, send it to everyone else (except same uid)...
-    for (&uid, tx) in users.read().await.iter() {
-        if user_id != uid {
-            if let Err(_disconnected) = tx.send(Message::text(new_msg.clone())) {
+    for user in users.read().await.iter() {
+        if user.id != user_id && user.room == room_id {
+            if let Err(_disconnected) = user.tx.send(Message::text(new_msg.clone())) {
                 // The tx is disconnected, our `user_disconnected` code
                 // should be happening in another task, nothing more to
                 // do here.
@@ -124,7 +122,8 @@ async fn user_message(user_id: usize, msg: Message, users: &Users) {
     }
 }
 
-static INDEX_HTML: &str = r#"<!DOCTYPE html>
+static INDEX_HTML: &str = r#"
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <title>Warp Chat</title>
@@ -132,36 +131,45 @@ static INDEX_HTML: &str = r#"<!DOCTYPE html>
     </head>
     <body>
         <h1>Warp chat</h1>
+        
         <div id="chat">
             <p><em>Connecting...</em></p>
         </div>
+        
         <input type="text" id="text" />
+        
         <button type="button" id="send">Send</button>
+        
         <script type="text/javascript">
-        const chat = document.getElementById('chat');
-        const text = document.getElementById('text');
-        const uri = `ws://${location.host}/chat/${1}`;
-        const ws = new WebSocket(uri);
-        function message(data) {
-            const line = document.createElement('p');
-            line.innerText = data;
-            chat.appendChild(line);
-        }
-        ws.onopen = function() {
-            chat.innerHTML = '<p><em>Connected!</em></p>';
-        };
-        ws.onmessage = function(msg) {
-            message(msg.data);
-        };
-        ws.onclose = function() {
-            chat.getElementsByTagName('em')[0].innerText = 'Disconnected!';
-        };
-        send.onclick = function() {
-            const msg = text.value;
-            ws.send(msg);
-            text.value = '';
-            message('<You>: ' + msg);
-        };
+            const chat = document.getElementById('chat');
+            const text = document.getElementById('text');
+            const uri = `ws://${location.host}/chat${location.pathname}`;
+            const ws = new WebSocket(uri);
+            
+            function message(data) {
+                const line = document.createElement('p');
+                line.innerText = data;
+                chat.appendChild(line);
+            }
+            
+            ws.onopen = function() {
+                chat.innerHTML = '<p><em>Connected!</em></p>';
+            };
+            
+            ws.onmessage = function(msg) {
+                message(msg.data);
+            };
+            
+            ws.onclose = function() {
+                chat.getElementsByTagName('em')[0].innerText = 'Disconnected!';
+            };
+            
+            send.onclick = function() {
+                const msg = text.value;
+                ws.send(msg);
+                text.value = '';
+                message('<You>: ' + msg);
+            };
         </script>
     </body>
 </html>
